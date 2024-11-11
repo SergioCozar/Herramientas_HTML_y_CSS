23110c85c72e05a5cb079f5fdecc4a327d1b7bfc
Sergio Cozar <sergiocozar.r@gmail.com>
2024-11-11 17:17:31 +0100

    Añadidos Header y Portada

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
-import "animate.css";
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.querySelector(".navbar__toggle");
+  const closeButton = document.querySelector(".navbar__close-toggle");
+  const menu = document.querySelector(".navbar__menu");
 
-console.log("Hello World!");
+  toggleButton.addEventListener("click", () => {
+    menu.classList.add("active");
+    toggleButton.classList.add("navbar__toggle--hidden");
+  });
+
+  closeButton.addEventListener("click", () => {
+    menu.classList.remove("active");
+    toggleButton.classList.remove("navbar__toggle--hidden");
+  });
+
+  menu.querySelectorAll(".menu-item a").forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("active");
+      toggleButton.classList.remove("navbar__toggle--hidden");
+    });
+  });
+});
