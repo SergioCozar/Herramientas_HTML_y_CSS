822c37656faa7587b6f22797ed0d9e43ba80602a
Sergio Cozar <sergiocozar.r@gmail.com>
2024-11-12 16:06:04 +0100

    Añadido enlace a Inicio a header

--- src/app.js
+++ src/app.js
@@ -2,8 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.querySelector(".navbar__toggle");
   const closeButton = document.querySelector(".navbar__close-toggle");
   const menu = document.querySelector(".navbar__menu");
+  const menuItems = menu.querySelectorAll(".menu-item a");
+
+  const getLastPathSegment = (url) => {
+    const path = new URL(url).pathname;
+    return path === "/" ? "/index.html" : path;
+  };
+
+  const currentPath = getLastPathSegment(window.location.href);
 
   toggleButton.addEventListener("click", () => {
+    console.log("clicked");
     menu.classList.add("active");
     toggleButton.classList.add("navbar__toggle--hidden");
   });
@@ -13,10 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
     toggleButton.classList.remove("navbar__toggle--hidden");
   });
 
-  menu.querySelectorAll(".menu-item a").forEach((link) => {
+  menuItems.forEach((link) => {
     link.addEventListener("click", () => {
       menu.classList.remove("active");
       toggleButton.classList.remove("navbar__toggle--hidden");
     });
+
+    if (getLastPathSegment(link.href) === currentPath) {
+      link.parentElement.classList.add("menu-item--active");
+    }
   });
 });
